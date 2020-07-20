4194af6bce4aa085e4a2c86c21581eed605f8ca2
saiqulhaq <saiqulhaq@gmail.com>
2020-07-20 13:44:44 +0700

    Init gem

---
+++ lib/activemerchant_promptpay.rb
@@ -0,0 +1,6 @@
+require "activemerchant_promptpay/version"
+
+module ActivemerchantPromptpay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/activemerchant_promptpay_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ActivemerchantPromptpay do
+  it "has a version number" do
+    expect(ActivemerchantPromptpay::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
